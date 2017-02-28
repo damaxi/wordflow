d4a009c2d1ebfa4336dde02df955b0bd3abe45cf
Martin <martin.hloska@gmail.com>
2017-02-28 14:19:07 +0100

    initial commit for firebaseclient

---
+++ firebaseClient/lib/include/firebaseclient.h
@@ -0,0 +1,18 @@
+//
+// Created by martin on 2/28/17.
+//
+
+#ifndef FIREBASECLIENT_FIREBASECLIENT_H
+#define FIREBASECLIENT_FIREBASECLIENT_H
+
+namespace FirebaseClient {
+
+    class FirebaseClient{
+
+    public:
+        FirebaseClient();
+
+    };
+}
+
+#endif //FIREBASECLIENT_FIREBASECLIENT_H
---
+++ firebaseClient/lib/src/firebaseclient.cpp
@@ -0,0 +1,10 @@
+//
+// Created by martin on 2/28/17.
+//
+
+#include "../include/firebaseclient.h"
+
+
+FirebaseClient::FirebaseClient::FirebaseClient() {
+
+}
